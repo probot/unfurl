0e06aab77674c95023a824a55ded60e1e475ea26
Brandon Keepers <bkeepers@github.com>
2018-03-08 21:49:48 -0600

    Update to probot@6

--- test/index.test.js
+++ test/index.test.js
@@ -6,7 +6,6 @@ const repo = require('fs').readFileSync('./test/fixtures/html/repo.html').toStri
 
 const nock = require('nock')
 const cacheManager = require('cache-manager')
-const GitHubApi = require('github')
 
 nock.disableNetConnect()
 nock.enableNetConnect(/127\.0\.0\.1/)
@@ -15,22 +14,11 @@ test('it works', async () => {
   // FIXME: move this and app setup below to a test harness
   const cache = cacheManager.caching({store: 'memory'})
 
-  const app = {
-    async asApp () {
-      return new GitHubApi()
-    },
-
-    async asInstallation () {
-      return new GitHubApi()
-    },
-
-    async createToken () {
-      return {data: {token: 'test'}}
-    }
-  }
+  const app = () => 'jwt'
   const robot = createRobot({app, cache})
 
   nock('https://api.github.com')
+    .post('/installations/13055/access_tokens').reply(200, {token: 'test'})
     .get('/repos/robotland/test/issues/comments/336888903').reply(200, comment)
     .patch('/repos/robotland/test/issues/comments/336888903').reply(200)
   nock('https://github.com')
